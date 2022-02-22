3e73b1d1799e2c4d5fbb7018909b30be6cfca83f
Ivan Frolov <ivan.frolov@mynd.co>
2022-02-22 17:35:24 +0300

    Removed unused directives

--- frontend/src/pages/files/files.module.ts
+++ frontend/src/pages/files/files.module.ts
@@ -4,7 +4,6 @@ import * as angular from 'angular';
 import { filesInputDirective } from './files-input/files-input.directive';
 import { filesRouting } from './files.module.routing';
 import { filesComponent } from './files/files.component';
-import { ngThumbDirective } from './ng-thumb/ng-thumb.directive';
 import { FilesService } from './services/files.service';
 import { uploadComponent } from './upload/upload.component';
 
@@ -12,7 +11,6 @@ const FILES_MODULE = angular
   .module('filesModule', [])
   .component('filesComponent', filesComponent)
   .component('uploadComponent', uploadComponent)
-  .directive('ngThumb', ngThumbDirective)
   .directive('filesInput', filesInputDirective)
   .service('FilesService', FilesService)
   .config(filesRouting);
